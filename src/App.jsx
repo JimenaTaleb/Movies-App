94a1ced08114c5e41a40214695143c2ec0ffe3b0
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-15 11:49:42 -0300

    adding mainsection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Header from "./components/Header";
+import MainSection from "./views/MainSection";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <MainSection/>
       <Footer />
     </>
   );
 }
 
 export default App;
+
+/* 
+Estructura App
+Header
+MainSection (para que la estructura html quede header-main-footer) => CarouselNowPlaying (1) CarouselPopular CarouselTopRated (1 reutilizable) views
+Upcoming views
+Popular views
+Search views
+MyList views
+MovieDetail views
+MovieTrailer views
+NotFound components
+Footer components
+
+MovieCard components
+
+*/
