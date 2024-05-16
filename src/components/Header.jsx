c0448a2a9489ad5549df25a6a9482ad7b2e5cae7
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-16 16:23:54 -0300

    adding links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
 import { Typography, Button, ButtonGroup } from "@mui/material/";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <Typography variant="h1">Movies App</Typography>
       <ButtonGroup variant="contained" aria-label="Basic button group">
-        <Button>Home</Button>
-        <Button>Últimos lanzamientos</Button>
-        <Button>Populares</Button>
-        <Button>Mi lista</Button>
-        <Button>Buscar</Button>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
+        <Link to="/upcoming">
+          <Button>Últimos lanzamientos</Button>
+        </Link>
+        <Link to="/popular">
+          <Button>Populares</Button>
+        </Link>
+        <Link>
+          <Button>Mi lista</Button>
+        </Link>
+        <Link>
+          <Button>Mi lista</Button>
+        </Link>
+        <Link>
+          <Button>Buscar</Button>
+        </Link>
       </ButtonGroup>
     </header>
   );
