e63936fdc1dde8c7113cb8f5ef48b614f305580e
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-17 20:40:48 -0300

    modifying notfound component and mylistcontext

--- src/context/MyListContext.jsx
+++ src/context/MyListContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from 'react';
 export const MyListContext = createContext();
 
 export const MyListProvider = ({ children }) => {
-  const [myList, setMyList] = useState([]);
+  const [myList, setMyList] = useState(null);
 
   useEffect(() => {
     const myListLS = JSON.parse(localStorage.getItem('myList'));
