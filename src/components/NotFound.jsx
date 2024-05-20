d47b7ee70455bd0b7087f4378f9af2118d25d1d6
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-20 09:24:54 -0300

    adding nofound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,16 +1,15 @@
-import notfound from "../assets/notfoundv3.jpg"
+import notfound from "../assets/notfoundv3.jpg";
 import { Link } from "react-router-dom";
 
 export default function NotFound() {
-    return (
-        <div>
-            <img src={notfound} alt="Page Not Found" />
-            <h2>Oh dear. Floo powders did not work properly?</h2>
-            <h3>Go back to the homepage.</h3>
-            <Link to={`/`} style={{ textDecoration: "none" }}>
+  return (
+    <div>
+      <img src={notfound} alt="Page Not Found" />
+      <h2>Oh dear! Floo powders did not work properly?</h2>
+      <h3>Go back to the homepage.</h3>
+      <Link to={`/`} style={{ textDecoration: "none" }}>
         <button>Volver</button>
       </Link>
-        </div>
-
-    );
+    </div>
+  );
 }
