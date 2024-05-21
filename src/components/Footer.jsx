6004b5f9570f7bfe8c2fad66068fe9662735e6e8
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-21 19:35:07 -0300

    modifying footer component styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,41 @@
+//Importo componentes de MUI
 import { Typography, Box } from "@mui/material";
+
+//Importo iconos de ReactIcons
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer style={{ backgroundColor: "#001529", padding: "10px 20px" }}>
       <Typography
+        align="center"
         sx={{
           fontSize: { xs: 15, md: 20, lg: 25 },
+          color: "#fff",
+          fontWeight: "bold",
+          mb: { xs: 2, md: 0 },
         }}
       >
         &copy; 2024 - by Jimena Taleb
       </Typography>
       <Box
-        pb={2}
+        pb={1}
         display="flex"
-        justifyContent="space-between"
+        justifyContent="center"
+        gap={13}
         sx={{
           fontSize: { xs: 15, md: 20, lg: 25 },
         }}
       >
         <a href="https://www.linkedin.com/in/jimenataleb">
-          <FaLinkedin color="black" />
+          <FaLinkedin color="#fff" />
         </a>
         <a href="https://github.com/JimenaTaleb">
-          <FaGithub color="black" />
+          <FaGithub color="#fff" />
         </a>
         <a href="mailto:jime-taleb@hotmail.com">
-          <MdAlternateEmail color="black" />
+          <MdAlternateEmail color="#fff" />
         </a>
       </Box>
     </footer>
