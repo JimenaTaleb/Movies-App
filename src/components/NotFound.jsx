9991880fd2ca8a676361637f5e15d2d841de8b83
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-21 11:27:47 -0300

    replacing link with usenavigate at NotFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,15 +1,16 @@
 import notfound from "../assets/notfoundv3.jpg";
-import { Link } from "react-router-dom";
+import { useNavigate  } from "react-router-dom";
 
 export default function NotFound() {
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <img src={notfound} alt="Page Not Found" />
       <h2>Oh dear! Floo powders did not work properly?</h2>
       <h3>Go back to the homepage.</h3>
-      <Link to={`/`} style={{ textDecoration: "none" }}>
-        <button>Volver</button>
-      </Link>
+      <button onClick={() => navigate(`/`)}>Volver</button>
     </div>
   );
 }
