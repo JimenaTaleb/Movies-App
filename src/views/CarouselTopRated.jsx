cedd8f5b340c0fbd4fb9e583ac2f1a31664ec28a
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-23 12:57:14 -0300

    reviewing and optimizing code at CarouselTopRated component

--- src/views/CarouselTopRated.jsx
+++ src/views/CarouselTopRated.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
-import CarouselMovie from "./CarouselMovie";
+//Importo useEffect
+import { useEffect } from "react";
+
+//Importo customHook
 import useMovies from "../hooks/useMovies";
 
-export default function CarouselPopular(){
+//Importo componente
+import CarouselMovie from "./CarouselMovie";
+
+export default function CarouselPopular() {
   const { movies, getMovies } = useMovies();
-  
+
   useEffect(() => {
     getMovies("top_rated", 1);
   }, []);
-  
+
   return (
     <CarouselMovie
       movies={movies.slice(0, 20)}
       title="Películas Mejor Ranqueadas"
     />
   );
-};
+}
