95e5b93ee43839e8044388a33d21eb68bde42493
Jimena Taleb <jime-taleb@hotmail.com>
2024-05-23 12:34:14 -0300

    fixing errors styles at TitleSection component

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -10,7 +10,7 @@ export default function TitleSection({ title }) {
         fontSize: { xs: 30, md: 40, lg: 50 },
         color: "#231841",
         fontWeight: "bold",
-        margin: "40 0",
+        margin: "40px 0px",
         textShadow: "2px 2px 2px #9cdbd4",
         textAlign: "center",
       }}
